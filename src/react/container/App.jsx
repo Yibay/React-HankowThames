cf4f59e1af26b443bc765f01db43d4702255c4ad
Yibay <514531536@qq.com>
2017-07-20 14:38:54 +0800

    在action上绑定dispatch

--- src/react/container/App.jsx
+++ src/react/container/App.jsx
@@ -2,8 +2,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { VisibilityFilters, addTodo } from '../actions/todoApp'
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from '../actions/todoApp'
 
+// 将 actions 打包成一个对象
+const actionCreators = { addTodo, toggleTodo, setVisibilityFilter };
 
 class App extends Component {
 	render () {
@@ -11,12 +13,13 @@ class App extends Component {
 		return <div>000</div>
 	}
 	componentDidMount () {
-		this.props.dispatch(addTodo('lalala'));
+		// this.props.dispatch(addTodo('lalala'));
+		this.props.addTodo('xixixi');
 	}
 }
 
-// 从 state 中，处理出 部分数据，传入 connect绑定的组件
-function select (state) {
+// 从 state 中，筛选出 部分数据，传入 connect绑定的组件
+function mapStateToProps (state) {
 	// 返回一个obj
 	return {
 		// 可见todo 数组
@@ -36,7 +39,10 @@ function select (state) {
 }
 
 // 通过 connect，把 state、dispatch 传入 组件(智能化 组件)
-	// 1、connect 向 select中，传入 state
-	// 2、select 返回值 作为 props 传入 App
-	// 3、同时 dispatch 也会一同 传入 App
-export default connect(select)(App);
+	// 1、mapStateToProps
+		// 不传 或 null，则 state, 不传入 App 的 props上
+		// 传，则 mapStateToProps 中传入state，返回 obj ，...obj 绑定到 App的 props上
+	// 2、actionCreators
+		// 不传 或 null，则 dispatch 传入 App 的 props上
+		// 传，则 actionCreators 各属性: action函数，被 绑定上 dispatch (可直接调用)，传入 到 App的 props 上
+export default connect(mapStateToProps, actionCreators)(App);
